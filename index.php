2bce930b15ac54cdf962c5f2fd24f1175fb6e2f9
Ajla <ajla.herenda@stu.ibu.edu.ba>
2023-02-28 18:08:05 +0100

    test commit

--- index.php
+++ index.php
@@ -3,5 +3,9 @@ require 'vendor/autoload.php';
 Flight::route('/', function (){
     echo "Hello World";
 });
+Flight::route('/', function() {
+    echo "Ajla";
+    echo "Faris";
+});
 Flight::start();
 ?>
