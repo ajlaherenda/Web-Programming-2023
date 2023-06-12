f7c51e11e4a8054f7b2ba2eee620742d09855206
Ajla <ajla.herenda@stu.ibu.edu.ba>
2023-06-13 00:40:39 +0200

    JUnit tests

--- tests/LocationDaoTest.php
+++ tests/LocationDaoTest.php
@@ -10,7 +10,14 @@ final class LocationDaoTest extends TestCase
     // in the test below we check if the connection has been established and further check if that is the proper table
     public function testDBConnection()
     {
-        $lcoationDao = new LocationDao("locations");
+        $locationDao = new LocationDao("locations");
         $this->assertSame('locations', $locationDao->getTableName());
+    }
+    // below we ensure that the name of the location at 71000 is actually Sarajevo
+    public function testGetById()
+    {
+        $locationDao = new LocationDao("locations");
+        $location = $locationDao->getById('71000');
+        $this->assertSame('Sarajevo', $location['location_name']);
     }
 }
